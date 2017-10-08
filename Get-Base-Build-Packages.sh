72ed49d4300688b2aa780f6dc02a7e415f270672
Joshua Flynn <flynn4anything@hotmail.com>
2017-10-08 23:54:35 +0100

    Add files via upload

---
+++ Get-Base-Build-Packages.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#Version 1.0
+
+#installpkg
+#removepkg
+
+#SlackWare
+
+installpkg bash
+installpkg binutils
+installpkg bison
+installpkg bzip2
+installpkg coreutils
+installpkg diffutils
+installpkg findutils
+installpkg gawk
+installpkg gcc
+installpkg g++
+installpkg glibc
+installpkg grep
+installpkg gzip
+installpkg m4
+installpkg make
+installpkg patch
+installpkg perl
+installpkg sed
+installpkg tar
+installpkg texinfo
+installpkg xz-utils
+
+#Debian
+
+apt-get install -y bash
+apt-get install -y binutils
+apt-get install -y bison
+apt-get install -y bzip2
+apt-get install -y coreutils
+apt-get install -y diffutils
+apt-get install -y findutils
+apt-get install -y gawk
+apt-get install -y gcc
+apt-get install -y g++
+apt-get install -y glibc
+apt-get install -y grep
+apt-get install -y gzip
+apt-get install -y m4
+apt-get install -y make
+apt-get install -y patch
+apt-get install -y perl
+apt-get install -y sed
+apt-get install -y tar
+apt-get install -y texinfo
+apt-get install -y xz-utils
+
